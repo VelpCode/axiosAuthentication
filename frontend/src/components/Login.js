b05e6596169d3075ba03fed783171f7eaa814d18
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-22 12:59:21 -0500

    setting up axious auth

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import { useNavigate, Link } from "react-router-dom"
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function submit(e) {
+        e.preventDefault()
+
+        try {
+
+            await axios
+
+        }
+
+
+
+    }
+
+  return (
+    <div className='login'>
+
+        <h1>Login</h1>
+
+        <form action="POST">
+            <input type = "email" onChange={(e) => {setEmail(e.target.value)}} placeholder='Email'/>
+            <input type = "password" onChange={(e) => {setPassword(e.target.value)}} placeholder='Password'/>
+            <input type = "submit" onClick={submit} />
+        </form>
+
+        <br />
+        <p>OR</p>
+        <br />
+
+        <Link to = "/signup"></Link>
+
+    </div>
+  )
+}
+
+export default Login
